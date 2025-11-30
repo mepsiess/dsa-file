c8636c38f9f686fed5ba4f0ef36d848a1d76b666
Mansi Gupta <gupta.mansi2910@gmail.com>
2025-11-30 14:19:01 +0530

    Update doublyLL.cpp

--- doublyLL.cpp
+++ doublyLL.cpp
@@ -1,3 +1,11 @@
+/* 2. Write a program to implement doubly linked list as an ADT that supports the following operations:
+i. Insert an element x at the beginning of the doubly linked list
+ii. Insert an element x at the end of the doubly linked list
+iii. Remove an element from the beginning of the doubly linked list
+iv. Remove an element from the end of the doubly linked list */
+
+
+
 #include <iostream>
 using namespace std;
 
