d0bb8f8d07218c5429fff20cfd95d53ca50f26c5
Mansi Gupta <gupta.mansi2910@gmail.com>
2025-11-30 14:17:45 +0530

    Update singlyLL.cpp

--- singlyLL.cpp
+++ singlyLL.cpp
@@ -1,3 +1,12 @@
+/* 1. Write a program to implement singly linked list as an ADT that supports the following operations: 
+i. Insert an element x at the beginning of the singly linked list
+ii. Insert an element x at ith position in the singly linked list
+iii. Remove an element from the beginning of the doubly linked list
+iv. Remove an element from ith position in the singly linked list.
+v. Search for an element x in the singly linked list and return its pointer*/
+
+
+
 #include <iostream>
 using namespace std;
 
